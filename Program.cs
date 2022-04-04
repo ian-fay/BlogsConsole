336f61a9a945799f9762fba8667dc285805fd48d
Ian Fay <ianfay2003@gmail.com>
2022-04-04 13:35:23 -0500

    1.02: Added displaying and creation of blogs

--- Program.cs
+++ Program.cs
@@ -13,33 +13,77 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
-            try
+            //Console.WriteLine("Hello World!");
+            string choice = "";
+
+                do
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                // ask user a question
+                Console.WriteLine("Enter your selection:");
+                Console.WriteLine("1) Display all blogs");
+                Console.WriteLine("2) Add Blog");
+                Console.WriteLine("3) Create Post");
+                Console.WriteLine("4) Display Posts");
+                Console.WriteLine("Enter 'q' to quit");
+                choice = Console.ReadLine();
+                
+                if (choice == "1")
                 {
-                    Console.WriteLine(item.Name);
+                    logger.Info("User choice: {Choice}", choice);
+                    try {
+                    var db = new BloggingContext();
+                    // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                        Console.WriteLine("\n");
+                    } catch(Exception ex) {
+                        logger.Error(ex.Message);
+                        Console.WriteLine("\n");
+                    }
+                    
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
+
+                else if (choice == "2")
+                {
+                    logger.Info("User choice: {Choice}", choice);
+                    try
+                    {
+
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog {Name = name};
+
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                        Console.WriteLine("\n");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                        Console.WriteLine("\n");
+                    }
+
+                }
+                else if(choice == "3") 
+                {          
+                    logger.Info("User choice: {Choice}", choice);
+
+                }
+                else if(choice == "4") 
+                {
+                    logger.Info("User choice: {Choice}", choice);
+                }
+
+            } while (choice == "1" || choice == "2" || choice == "3" || choice == "4");
 
             logger.Info("Program ended");
         }
